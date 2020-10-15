0260453d1e121628d35f85531f4b3cdf5300fb59
nayaradias <nayaradias@users.noreply.github.com>
2020-10-15 19:19:28 -0300

    update

---
+++ front/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+import styles from "./styles";
+
+const Header = () => {
+  return <View />;
+};
+
+export default Header;
---
+++ front/src/components/Header/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default styles;
