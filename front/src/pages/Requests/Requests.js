8e9d90f83b7923e5bd2652d4dc81c91f08ab64bf
Humberto Tozetti Carlos <tozettihumberto@gmail.com>
2020-11-23 20:05:30 -0300

    Criada tela de Pedidos
    Criada tela de Pedidos

--- front/src/pages/Requests/Requests.js
+++ front/src/pages/Requests/Requests.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
+import React from "react";
+import { Container, Title, Text } from "../../style/global/general";
+import { FontAwesome } from "@expo/vector-icons";
+import colors from "../../style/global/colors";
+import {
+  Content,
+  Card,
+  ContentText,
+  ContainerText,
+  Banner,
+  Row,
+  Column,
+} from "../../style/pages/requests";
 
 const Requests = () => {
-    return (<View ><Text>Requests</Text></View>);
-}
+  
+  return (
+    <Container>
+      <Content>
+        <Card>
+          <Row>
+            <Banner
+              source={require("../../assets/images/espetinho-carne.jpg")}
+            />
+            <Column>
+              <>
+                <ContentText>
+                  <Text marginLeft={10}>
+                      23/11/20 19:49
+                  </Text>
+                  <Title fontWeight={500} marginLeft={10} color={colors.gray}>
+                    Espetinho de Boi
+                  </Title>
+                </ContentText>
+                <Text marginLeft={11} color={colors.lightgray}>
+                  250g/Un
+                </Text>
+              </>
+              <ContainerText>
+                <Text fontWeight="bold" marginLeft={11} color={colors.gray}>
+                  R$ 28,00
+                </Text>
+                <Text fontWeight="bold" marginLeft={60} color={colors.orange}>
+                  Rastrear Pedido
+                </Text>
+              </ContainerText>
+            </Column>
+          </Row>
+        </Card>
+      </Content>
+    </Container>
+  );
+};
 
 export default Requests;
