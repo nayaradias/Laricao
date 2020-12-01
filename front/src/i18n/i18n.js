3a990590080fff602f5928de402a80729d9fc625
BrunoBonadiman <bruno.bonadiman@autoglass.com.br>
2020-11-30 22:39:11 -0300

    translate
    translate

---
+++ front/src/i18n/i18n.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import intl from 'react-intl-universal';
+
+
+const localizedStrings = {
+    'en_US': { title: "Hello", subtitle: "Welcome"},
+    'es_ES': { title: "Hola", subtitle: "Bienvenido"},
+    'pt-BR': { title: "Olá", subtitle: "Bem-Vindo"}
+}
